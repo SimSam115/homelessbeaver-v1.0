0bb2a9b64bd90f37b4cc72aba80c796673ce0e44
SimSam115 <haffnersam@gmail.com>
2021-09-23 19:38:39 -0500

    working on play functions

--- src/main/java/Controllers/gameController.java
+++ src/main/java/Controllers/gameController.java
@@ -1,6 +1,9 @@
 package Controllers;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,15 @@ public class gameController {
 
 	@GetMapping({"/game"})
 	public String entrance(Model model) {
-		System.out.println("loaded");
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(14);
+		list.add(15);
+		
+		
+		model.addAttribute("number", list);
 		
 		return "testGame";
 	}
---
+++ src/main/java/data/GameSavedData.java
@@ -0,0 +1,13 @@
+package data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class GameSavedData {
+	String data;
+	int id;
+}
